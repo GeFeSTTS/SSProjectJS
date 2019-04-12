a4d083b05a4c31d927e9a484ee4a5daf622eb6b4
Alex Domianich <alexshadow55@gmail.com>
2019-04-12 09:57:09 +0000

    fix

--- scripts/converter.js
+++ scripts/converter.js
@@ -1,30 +1,30 @@
+const to = document.getElementById("to");
 document.querySelector('select').addEventListener('change', showConvert);
 
 function showConvert() {
     fetch('products.json')
-        .then( result => result.json)
+        .then( result => result.json())
         .then(prods => convertion(prods));
     
     function convertion(products) {
-        console.log(products);
+        const currTo = to.options[to.selectedIndex].value;
+        const currFrom = products[0].currency;
+        const currKey = currFrom + '_' + currTo;
+        products.forEach(function(products){
+            fetch(`https://free.currencyconverterapi.com/api/v6/convert?q=${currKey}&compact=ultra&apiKey=6f0e3f1d07f5b51eb161`)
+                .then( response => response.json() )
+                .then( currency => {
+                    const rate = currency[currKey];
+                    const sourceAmount = products.price;
+                    const convertedAmount = rate * sourceAmount;
+                    products.price = convertedAmount.toFixed(2);
+                    products.currency = currTo;
+            })
+            renderPage()
+        })
     }
-};
-
-/* function showConvert(){
-    const to = document.getElementById("to");
-    const currTo = to.options[to.selectedIndex].value;
-    const from = products.currency;
-    console.log(from);
-    const val = document.querySelectorAll(".price p");
-    const currFrom = from[1];
-    const currKey = currFrom + '_' + currTo; 
-    fetch(`https://free.currencyconverterapi.com/api/v6/convert?q=${currKey}&compact=ultra&apiKey=6f0e3f1d07f5b51eb161`)
-    .then( response => response.json() )
-    .then( currency => {
-       const rate = currency[currKey];
-       const convertedAmount = 0;
-       val.forEach(function(){
-        convertedAmount = rate * val;
-        val.innerText = convertedAmount.toFixed(2);
-       })
-    }); */
+    
+    function renderPage() {
+        $(".content").load(index.html + " .content .price p");
+    }
+}
